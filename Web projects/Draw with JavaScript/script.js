e5e11a2976f435f1aba12b86700d6850954a3d61
Edvin <ummahusla@gmail.com>
2013-10-06 13:03:02 +0100

    Code for the exercise

---
+++ Web projects/Draw with JavaScript/script.js
@@ -0,0 +1,35 @@
+// Set up!
+var a_canvas = document.getElementById("a");
+var context = a_canvas.getContext("2d");
+
+// Draw the face
+context.fillStyle = "yellow";
+context.beginPath();
+context.arc(95, 85, 40, 0, 2*Math.PI);
+context.closePath();
+context.fill();
+context.lineWidth = 2;
+context.stroke();
+context.fillStyle = "black";
+
+// Draw the left eye
+context.beginPath();
+context.arc(75, 75, 5, 0, 2*Math.PI);
+context.closePath();
+context.fill();
+
+// Draw the right eye
+context.beginPath();
+context.arc(114, 75, 5, 0, 2*Math.PI);
+context.closePath();
+context.fill();
+
+// Draw the mouth
+context.beginPath();
+context.arc(95, 90, 26, Math.PI, 2*Math.PI, true);
+context.closePath();
+context.fill();
+
+// Write "Hello, World!"
+context.font = "30px Garamond";
+context.fillText("Hello, World!",15,175);

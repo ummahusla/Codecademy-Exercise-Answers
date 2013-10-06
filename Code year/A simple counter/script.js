91c2b693421b25b4f9840d64ca4cdb0882920486
Edvin <ummahusla@gmail.com>
2013-10-06 13:27:47 +0100

    Code for the exercise

---
+++ Code year/A simple counter/script.js
@@ -0,0 +1,11 @@
+function modify_qty(val) {
+    var qty = document.getElementById('qty').value;
+    var new_qty = parseInt(qty,10) + val;
+    
+    if (new_qty < 0) {
+        new_qty = 0;
+    }
+    
+    document.getElementById('qty').value = new_qty;
+    return new_qty;
+}

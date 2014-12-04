341b361ab09a3020fefd6862e665f38b2050ba9f
Edvins Antonovs <ummahusla@gmail.com>
2014-12-04 15:57:52 +0000

    Typo

--- Python/PygLatin.py
+++ Python/PygLatin.py
@@ -1,8 +1,6 @@
-'''
-Pig Latin is a language game, where you move the first letter
-of the word to the end and add "ay." So "Python" becomes
-"ythonpay."
-'''
+#Pig Latin is a language game, where you move the first letter
+#of the word to the end and add "ay." So "Python" becomes
+#"ythonpay."
 
 pyg = 'ay'
 

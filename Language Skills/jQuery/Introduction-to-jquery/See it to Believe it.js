39b3a0c1182ed0522e8a49e87ba829f4e56570ca
Jalu1 <Jalu1@users.noreply.github.com>
2015-10-08 12:33:33 -0700

    Create See it to Believe it.js

---
+++ Language Skills/jQuery/Introduction-to-jquery/See it to Believe it.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+   $('div').mouseenter(function() {
+       $(this).animate({
+           height: '+=10px'
+       });
+   });
+   $('div').mouseleave(function() {
+       $(this).animate({
+           height: '-=10px'
+       }); 
+   });
+   $('div').click(function() {
+       $(this).toggle(1000);
+   }); 
+});

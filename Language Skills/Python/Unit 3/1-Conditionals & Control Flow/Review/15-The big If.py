41003e0628057c8fd02d4ae73f714cb8b9d57801
Moulick Aggarwal <Moulick@users.noreply.github.com>
2016-07-13 11:38:31 +0530

    fixes #56
    corrected the code

--- Language Skills/Python/Unit 3/1-Conditionals & Control Flow/Review/15-The big If.py
+++ Language Skills/Python/Unit 3/1-Conditionals & Control Flow/Review/15-The big If.py
@@ -1,12 +1,12 @@
 # Make sure that the_flying_circus() returns True
 def the_flying_circus():
-    if True or True:    # Start coding here!
+    if 10 < 11 or 5<(6+1):    # Start coding here!
         return True # Don't forget to indent
         # the code inside this block!
     elif 7 >= 2:
-        return 'nooo' # Keep going here.
+        return False # Keep going here.
         # You'll want to add the else statement, too!
     else:
-        return 'non'
+        return True
 
 print the_flying_circus()

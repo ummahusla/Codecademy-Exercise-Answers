8a32768816e596bc50ec45406a50b9b241e712fa
Majda Puaca <supermajda@gmail.com>
2017-09-02 12:48:13 -0400

    Update places.js

--- Web Developer Skills/Learn AngularJS/Unit 5/NearMe 2/places.js
+++ Web Developer Skills/Learn AngularJS/Unit 5/NearMe 2/places.js
@@ -1,9 +1,7 @@
 app.factory('places', ['$http', function($http) {
-  return $http.jsonp('https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=5000&gscoord=40.741934%7C-74.004897&gslimit=30&format=json&callback=JSON_CALLBACK')
-    .success(function(data) {
-    return data;
-    })
-        .error(function(err) {
-    return err;
-    });
+	return $http.jsonp('https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=5000&gscoord=40.741934%7C-74.004897&gslimit=30&format=json&callback=JSON_CALLBACK').success(function(data) {
+		return data;
+	}).error(function(err) {
+		return err;
+	});
 }]);
